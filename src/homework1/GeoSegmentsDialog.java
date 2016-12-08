25409e3469aeb910c942dd2adf701608481409e4
Fayez Nassar <fayeznas@gmail.com>
2016-12-08 18:13:22 +0200

    Part 2, Route Formatter GUI.

--- src/homework1/GeoSegmentsDialog.java
+++ src/homework1/GeoSegmentsDialog.java
@@ -1,8 +1,9 @@
 package homework1;
 
-import java.awt.Frame;
-import javax.swing.JDialog;
-import javax.swing.JList;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
 /**
  * A JDailog GUI for choosing a GeoSegemnt and adding it to the route shown
@@ -30,9 +31,61 @@ public class GeoSegmentsDialog extends JDialog {
 		// in one that doesn't allow other windows to be active at the
 		// same time).
 		super(owner, "Please choose a GeoSegment", true);
-		
+
 		this.parent = pnlParent;
-		
-		// TODO Write the body of this method
+        JButton button = new JButton("Add");
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+
+        DefaultListModel listModel = new DefaultListModel();
+		GeoSegment[] segments = ExampleGeoSegments.segments;
+		for(int i = 0; i < segments.length ; i++) {
+			listModel.addElement(segments[i]);
+		}
+		lstSegments = new JList<>(listModel);
+        JScrollPane segmentListPane = new JScrollPane(lstSegments);
+        segmentListPane.setPreferredSize(new Dimension(440, 400));
+        segmentListPane.setAlignmentX(LEFT_ALIGNMENT);
+
+        JPanel segmentListPanel = new JPanel();
+        segmentListPanel.setLayout(new BoxLayout(segmentListPanel, BoxLayout.Y_AXIS));
+        JLabel geoSegmentLabel = new JLabel("GeoSegments");
+        geoSegmentLabel.setLabelFor(segmentListPanel);
+        segmentListPanel.add(geoSegmentLabel);
+        segmentListPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+        segmentListPanel.add(segmentListPane);
+        segmentListPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10,10));
+
+        JPanel buttonsPanel = new JPanel();
+        JButton addButton = new JButton("Add");
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pnlParent.addSegment((GeoSegment) lstSegments.getSelectedValue());
+            }
+        });
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.X_AXIS));
+        buttonsPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        buttonsPanel.add(addButton);
+        buttonsPanel.add(Box.createHorizontalGlue());
+        buttonsPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+        buttonsPanel.add(cancelButton);
+
+
+
+        getContentPane().add(segmentListPanel, BorderLayout.CENTER);
+        getContentPane().add(buttonsPanel, BorderLayout.SOUTH);
+        pack();
 	}
 }
--- src/homework1/RouteFormatterGUI.java
+++ src/homework1/RouteFormatterGUI.java
@@ -13,149 +13,171 @@ import java.awt.event.*;
  */
 public class RouteFormatterGUI extends JPanel {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private Route route = null;				// Route shown in this
+    private Route route = null;                // Route shown in this
 
-	private GeoSegmentsDialog dlgSegments;	// secondary window
+    private GeoSegmentsDialog dlgSegments;    // secondary window
 
-	// some of the controls contained in this
-	private JList<GeoSegment> lstSegments;
-	private JTextArea txtWalkingDirections;
-	private JTextArea txtDrivingDirections;
-
-
-	/**
-	 * Creates a new RoutFormatterGUI JPanel.
-	 * @effects Creates a new RoutFormatterGUI JPanel contained in frame.
-	 */
-	public RouteFormatterGUI(JFrame frame) {
-		// create a GeoSegmentsDialog (secondary window)
-		dlgSegments = new GeoSegmentsDialog(frame, this);
-		dlgSegments.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		dlgSegments.pack();
-
-		// create components
-		lstSegments = new JList<>(new DefaultListModel<GeoSegment>());
-		lstSegments.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		JScrollPane scrlSegments = new JScrollPane(lstSegments);
-		scrlSegments.setPreferredSize(new Dimension(450, 100));
-
-		JLabel lblSegments = new JLabel("Route's GeoSegments:");
-		lblSegments.setLabelFor(lstSegments);
-
-		txtWalkingDirections = new JTextArea();
-		txtWalkingDirections.setEditable(false);
-		JScrollPane scrlWalkingDirections = new JScrollPane(txtWalkingDirections);
-		scrlWalkingDirections.setPreferredSize(new Dimension(400, 70));
-		JLabel lblWalkingDirections = new JLabel("Walking Directions:");
-		lblWalkingDirections.setLabelFor(txtWalkingDirections);
-
-		txtDrivingDirections = new JTextArea();
-		txtDrivingDirections.setEditable(false);
-		JScrollPane scrlDrivingDirections = new JScrollPane(txtDrivingDirections);
-		scrlDrivingDirections.setPreferredSize(new Dimension(400, 70));
-		JLabel lblDrivingDirections = new JLabel("Driving Directions:");
-		lblDrivingDirections.setLabelFor(txtDrivingDirections);
-
-		JButton btnAddSegment = new JButton("Add GeoSegment");
-		btnAddSegment.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				dlgSegments.setVisible(true);
-			}
-		});
-
-		// arrange components on grid
-		this.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-		GridBagLayout gridbag = new GridBagLayout();
-		GridBagConstraints c = new GridBagConstraints();
-		this.setLayout(gridbag);
-
-		c.fill = GridBagConstraints.BOTH;
-
-		c.gridx = 0;
-		c.gridy = 0;
-		c.gridwidth = 1;
-		c.gridheight = 1;
-		c.insets = new Insets(0,0,0,0);
-		gridbag.setConstraints(lblSegments, c);
-		this.add(lblSegments);
-
-		c.gridx = 0;
-		c.gridy = 1;
-		c.gridwidth = 1;
-		c.gridheight = 5;
-		c.insets = new Insets(0,0,0,0);
-		gridbag.setConstraints(scrlSegments, c);
-		this.add(scrlSegments);
-
-		c.gridx = 1;
-		c.gridy = 0;
-		c.gridwidth = 1;
-		c.gridheight = 1;
-		c.insets = new Insets(0,20,0,0);
-		gridbag.setConstraints(lblWalkingDirections, c);
-		this.add(lblWalkingDirections);
-
-		c.gridx = 1;
-		c.gridy = 1;
-		c.gridwidth = 2;
-		c.gridheight = 1;
-		c.insets = new Insets(0,20,20,0);
-		gridbag.setConstraints(scrlWalkingDirections, c);
-		this.add(scrlWalkingDirections);
-
-		c.gridx = 1;
-		c.gridy = 2;
-		c.gridwidth = 2;
-		c.gridheight = 1;
-		c.insets = new Insets(0,20,0,0);
-		gridbag.setConstraints(lblDrivingDirections, c);
-		this.add(lblDrivingDirections);
-
-		c.gridx = 1;
-		c.gridy = 3;
-		c.gridwidth = 2;
-		c.gridheight = 1;
-		c.insets = new Insets(0,20,20,0);
-		gridbag.setConstraints(scrlDrivingDirections, c);
-		this.add(scrlDrivingDirections);
-
-		c.gridx = 2;
-		c.gridy = 4;
-		c.gridwidth = 1;
-		c.gridheight = 1;
-		c.insets = new Insets(0,20,0,0);
-		c.anchor = GridBagConstraints.SOUTH;
-		gridbag.setConstraints(btnAddSegment, c);
-		this.add(btnAddSegment);
-	}
+    // some of the controls contained in this
+    private JList<GeoSegment> lstSegments;
+    private JTextArea txtWalkingDirections;
+    private JTextArea txtDrivingDirections;
 
 
-	/**
-	 * Add new segment to the Route shown by this.
-	 * @requires segments != null && segment.p1 == this.route.end
-	 * @effects Add new segment to the end of the route shown by this as
-	 * 			defined by Route.addSegment(). In addition, updates the
-	 * 			walking direction and the driving direction of the GUI
-	 * 			with the return value of
-	 * 			RouteDirection.computeDirections(this.route,0)
-	 */
-	public void addSegment(GeoSegment segment) {
-		DefaultListModel<GeoSegment> model =
-				(DefaultListModel<GeoSegment>)(this.lstSegments.getModel());
-		
-		// TODO Write the body of this method
-	}
+    /**
+     * Creates a new RoutFormatterGUI JPanel.
+     *
+     * @effects Creates a new RoutFormatterGUI JPanel contained in frame.
+     */
+    public RouteFormatterGUI(JFrame frame) {
+        // create a GeoSegmentsDialog (secondary window)
+        dlgSegments = new GeoSegmentsDialog(frame, this);
+        dlgSegments.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        dlgSegments.pack();
+
+        // create components
+        lstSegments = new JList<>(new DefaultListModel<GeoSegment>());
+        lstSegments.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        JScrollPane scrlSegments = new JScrollPane(lstSegments);
+        scrlSegments.setPreferredSize(new Dimension(450, 100));
+
+        JLabel lblSegments = new JLabel("Route's GeoSegments:");
+        lblSegments.setLabelFor(lstSegments);
+
+        txtWalkingDirections = new JTextArea();
+        txtWalkingDirections.setEditable(false);
+        JScrollPane scrlWalkingDirections = new JScrollPane(txtWalkingDirections);
+        scrlWalkingDirections.setPreferredSize(new Dimension(400, 70));
+        JLabel lblWalkingDirections = new JLabel("Walking Directions:");
+        lblWalkingDirections.setLabelFor(txtWalkingDirections);
+
+        txtDrivingDirections = new JTextArea();
+        txtDrivingDirections.setEditable(false);
+        JScrollPane scrlDrivingDirections = new JScrollPane(txtDrivingDirections);
+        scrlDrivingDirections.setPreferredSize(new Dimension(400, 70));
+        JLabel lblDrivingDirections = new JLabel("Driving Directions:");
+        lblDrivingDirections.setLabelFor(txtDrivingDirections);
+
+        JButton btnAddSegment = new JButton("Add GeoSegment");
+        btnAddSegment.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dlgSegments.setVisible(true);
+            }
+        });
+
+        // arrange components on grid
+        this.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        GridBagLayout gridbag = new GridBagLayout();
+        GridBagConstraints c = new GridBagConstraints();
+        this.setLayout(gridbag);
+
+        c.fill = GridBagConstraints.BOTH;
+
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.insets = new Insets(0, 0, 0, 0);
+        gridbag.setConstraints(lblSegments, c);
+        this.add(lblSegments);
+
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 1;
+        c.gridheight = 5;
+        c.insets = new Insets(0, 0, 0, 0);
+        gridbag.setConstraints(scrlSegments, c);
+        this.add(scrlSegments);
+
+        c.gridx = 1;
+        c.gridy = 0;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.insets = new Insets(0, 20, 0, 0);
+        gridbag.setConstraints(lblWalkingDirections, c);
+        this.add(lblWalkingDirections);
+
+        c.gridx = 1;
+        c.gridy = 1;
+        c.gridwidth = 2;
+        c.gridheight = 1;
+        c.insets = new Insets(0, 20, 20, 0);
+        gridbag.setConstraints(scrlWalkingDirections, c);
+        this.add(scrlWalkingDirections);
+
+        c.gridx = 1;
+        c.gridy = 2;
+        c.gridwidth = 2;
+        c.gridheight = 1;
+        c.insets = new Insets(0, 20, 0, 0);
+        gridbag.setConstraints(lblDrivingDirections, c);
+        this.add(lblDrivingDirections);
+
+        c.gridx = 1;
+        c.gridy = 3;
+        c.gridwidth = 2;
+        c.gridheight = 1;
+        c.insets = new Insets(0, 20, 20, 0);
+        gridbag.setConstraints(scrlDrivingDirections, c);
+        this.add(scrlDrivingDirections);
+
+        c.gridx = 2;
+        c.gridy = 4;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.insets = new Insets(0, 20, 0, 0);
+        c.anchor = GridBagConstraints.SOUTH;
+        gridbag.setConstraints(btnAddSegment, c);
+        this.add(btnAddSegment);
+    }
+
+
+    /**
+     * Add new segment to the Route shown by this.
+     *
+     * @requires segments != null && segment.p1 == this.route.end
+     * @effects Add new segment to the end of the route shown by this as
+     * defined by Route.addSegment(). In addition, updates the
+     * walking direction and the driving direction of the GUI
+     * with the return value of
+     * RouteDirection.computeDirections(this.route,0)
+     */
+    public void addSegment(GeoSegment segment) {
+        DefaultListModel<GeoSegment> model =
+                (DefaultListModel<GeoSegment>) (this.lstSegments.getModel());
+        if (segment == null) {
+            return;
+        }
+
+        if (route == null) {
+            route = new Route(segment);
+        } else {
+            if (!segment.getP1().equals(route.endingGeoSegment.getP2())) {
+                return;
+            }
+            route = route.addSegment(segment);
+        }
+        DrivingRouteFormatter drivingRouteFormatter = new DrivingRouteFormatter();
+        WalkingRouteFormatter walkingRouteFormatter = new WalkingRouteFormatter();
+
+        String drivingRoute = drivingRouteFormatter.computeDirections(route, 0.0);
+        String walkingRoute = walkingRouteFormatter.computeDirections(route, 0.0);
+
+        model.addElement(segment);
+        txtDrivingDirections.setText(drivingRoute);
+        txtWalkingDirections.setText(walkingRoute);
+
+    }
 
 
     public static void main(String[] args) {
-		JFrame frame = new JFrame("Route Formatter GUI");
-		Container contentPane = frame.getContentPane();
-		contentPane.add(new RouteFormatterGUI(frame));
+        JFrame frame = new JFrame("Route Formatter GUI");
+        Container contentPane = frame.getContentPane();
+        contentPane.add(new RouteFormatterGUI(frame));
 
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
-		frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
